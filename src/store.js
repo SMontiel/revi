5cfda1592bcad6714f74cc5f5f9a7bc1a903cbf1
Salvador Montiel <salvadormontiel8@gmail.com>
2019-11-02 21:03:29 -0600

    :sparkles: Sync url and hash in webviews

--- src/store.js
+++ src/store.js
@@ -15,7 +15,11 @@ export default new Vuex.Store({
         width: 1200,
         height: 980
       }
-    ]
+    ],
+    isScrollEnabled: true,
+    host: 'https://laravel.com/docs/routing'
+    //host: 'https://vuejs.org/'
+    //host: 'http://localhost:8000/'
   },
   mutations: {
     addFrame (state, payload) {
@@ -27,6 +31,13 @@ export default new Vuex.Store({
     },
     deleteFrame (state, payload) {
       state.devicesData.splice(payload.index, 1);
+    },
+    setHost (state, payload) {
+      state.isScrollEnabled = false;
+      state.host = payload.newHost;
+      setTimeout(function () {
+        state.isScrollEnabled = true;
+      }, 500);
     }
   },
   actions: {},
